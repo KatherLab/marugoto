32a60ade98d4af0d785a16662c609dec38b93b64
Marko van Treeck <mvantreeck@ukaachen.de>
2022-04-26 10:42:08 +0200

    Add cross-validation to tile feature module.

--- marugoto/features/__init__.py
+++ marugoto/features/__init__.py
@@ -1,3 +1,6 @@
+from ._features import *
+
+
 __author__ = 'Marko van Treeck'
 __copyright__ = 'Copyright 2022, Kather Lab'
 __license__ = 'MIT'
@@ -6,6 +9,6 @@ __maintainer__ = 'Marko van Treeck'
 __email__ = 'mvantreeck@ukaachen.de'
 
 __changelog__ = {
-    '0.2.0': 'marugoto.features 0.1.0 -> 0.2.0',
-    '0.3.0': 'marugoto.features 0.2.0 -> 0.3.0',
+    '0.2.0': 'Add CLI for training and deploying models',
+    '0.3.0': 'Add cross-validation',
 }
--- marugoto/features/_features.py
+++ marugoto/features/_features.py
@@ -16,27 +16,13 @@ import torch.nn.functional as F
 from fastai.vision.all import (
     create_head, Learner, RocAuc,
     SaveModelCallback, EarlyStoppingCallback, CSVLogger,
-    DataLoader, DataLoaders, load_learner)
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+    DataLoader, DataLoaders)
 
-from .data import ZipDataset, EncodedDataset
+from ..data import ZipDataset, EncodedDataset
 
-
-__author__ = 'Marko van Treeck'
-__copyright__ = 'Copyright 2022, Kather Lab'
-__license__ = 'MIT'
-__version__ = '0.2.0'
-__maintainer__ = 'Marko van Treeck'
-__email__ = 'mvantreeck@ukaachen.de'
-
-__changelog__ = {
-    '0.2.0': 'Add CLI for training and deploying models.',
-}
 
 __all__ = [
-    'make_dataset', 'H5TileDataset', 'train',
-    'train_categorical_model_cli', 'deploy_categorical_model_cli']
+    'make_dataset', 'H5TileDataset', 'train', 'deploy']
 
 
 T = TypeVar('T')
@@ -103,100 +89,6 @@ class H5TileDataset(Dataset):
             len(f['feats'])
 
 
-def train_categorical_model_cli(
-        clini_excel: PathLike,
-        slide_csv: PathLike,
-        feature_dir: PathLike,
-        target_label: str,
-        output_path: PathLike,
-        categories=None,
-) -> None:
-    """Train a categorical model on a cohort's tile's features.
-
-    Args:
-        clini_excel:  Path to the clini table.
-        slide_csv:  Path to the slide tabel.
-        target_label:  Label to train for.
-        categories:  Categories to train for, or all categories appearing in the
-            clini table if none given (e.g. '["MSIH", "nonMSIH"]').
-        feature_dir:  Path containing the features.
-        output_path:  File to save model in.
-    """
-    feature_dir = Path(feature_dir)
-    output_path = Path(output_path)
-    output_path.mkdir(exist_ok=True, parents=True)
-
-    # just a big fat object to dump all kinds of info into for later reference
-    # not used during actual training
-    from datetime import datetime
-    info = {
-        'description': 'training on tile features',
-        'clini': str(clini_excel.absolute()),
-        'slide': str(slide_csv.absolute()),
-        'feature_dir': str(feature_dir.absolute()),
-        'target_label': str(target_label),
-        'output_path': str(output_path.absolute()),
-        'datetime': datetime.now().astimezone().isoformat()}
-
-    model_path = output_path/'export.pkl'
-    if model_path.exists():
-        print(f'{model_path} already exists. Skipping...')
-        return
-
-    clini_df = pd.read_excel(clini_excel)
-    slide_df = pd.read_csv(slide_csv)
-    df = clini_df.merge(slide_df, on='PATIENT')
-
-    # filter na, infer categories if not given
-    df = df.dropna(subset=target_label)
-
-    if not categories:
-        categories = df[target_label].unique()
-        print(f'Inferred {categories=}')
-    categories = np.array(categories)
-    info['categories'] = list(categories)
-
-    df = df[df[target_label].isin(categories)]
-
-    slides = set(feature_dir.glob('*.h5'))
-    # remove slides we don't have
-    slide_df = pd.DataFrame(slides, columns=['slide_path'])
-    slide_df['FILENAME'] = slide_df.slide_path.map(lambda p: p.stem)
-    df = df.merge(slide_df, on='FILENAME')
-
-    print('Overall distribution')
-    print(df[target_label].value_counts())
-    info['class distribution'] = {'overall': {
-        k: int(v) for k, v in df[target_label].value_counts().items()}}
-
-    # Split off validation set
-    patient_df = df.groupby('PATIENT').first()
-
-    train_patients, valid_patients = train_test_split(
-        patient_df.index, stratify=patient_df[target_label])
-    train_df = df[df.PATIENT.isin(train_patients)]
-    valid_df = df[df.PATIENT.isin(valid_patients)]
-
-    info['class distribution']['training'] = {
-        k: int(v) for k, v in train_df[target_label].value_counts().items()}
-    info['class distribution']['validation'] = {
-        k: int(v) for k, v in valid_df[target_label].value_counts().items()}
-
-    target_enc = OneHotEncoder(sparse=False).fit(categories.reshape(-1, 1))
-
-    with open(output_path/'info.json', 'w') as f:
-        json.dump(info, f)
-
-    learn = train(
-        target_enc=target_enc,
-        train_bags=train_df.slide_path.values,
-        train_targets=train_df[target_label].values,
-        valid_bags=valid_df.slide_path.values,
-        valid_targets=valid_df[target_label].values,
-        path=output_path)
-
-    learn.export(model_path)
-
 
 def train(
     *,
@@ -257,51 +149,14 @@ def train(
     return learn
 
 
-def deploy_categorical_model_cli(
-        clini_excel: PathLike,
-        slide_csv: PathLike,
-        feature_dir: PathLike,
-        target_label: str,
-        model_path: PathLike,
-        output_path: PathLike,
-) -> None:
-    """Deploy a categorical model on a cohort's tile's features.
-
-    Args:
-        clini_excel:  Path to the clini table.
-        slide_csv:  Path to the slide tabel.
-        target_label:  Label to train for.
-        feature_dir:  Path containing the features.
-        model_path:  Path of the model to deploy.
-        output_path:  File to save model in.
-    """
-    feature_dir = Path(feature_dir)
-    model_path = Path(model_path)
-    output_path = Path(output_path)
-    if (preds_csv := output_path/'patient-preds.csv').exists():
-        print(f'{preds_csv} already exists!  Skipping...')
-        return
-
-    clini_df = pd.read_excel(clini_excel)
-    slide_df = pd.read_csv(slide_csv)
-    test_df = clini_df.merge(slide_df, on='PATIENT')
-
-    learn = load_learner(model_path)
+def deploy(test_df, learn, target_label):
     target_enc = learn.dls.train.dataset._datasets[-1].encode
     categories = target_enc.categories_[0]
-
-    # remove uninteresting
-    test_df = test_df[test_df[target_label].isin(categories)]
-    # remove slides we don't have
-    slides = set(feature_dir.glob('*.h5'))
-    slide_df = pd.DataFrame(slides, columns=['slide_path'])
-    slide_df['FILENAME'] = slide_df.slide_path.map(lambda p: p.stem)
-    test_df = test_df.merge(slide_df, on='FILENAME')
 
     test_ds = make_dataset(
         target_enc=target_enc,
         bags=test_df.slide_path.values,
-        targets=test_df.isMSIH.values)
+        targets=test_df[target_label].values)
 
     test_dl = DataLoader(
         test_ds, batch_size=512, shuffle=False, num_workers=os.cpu_count())
@@ -339,13 +194,4 @@ def deploy_categorical_model_cli(
         *(f'{target_label}_{cat}' for cat in categories),
         'loss']]
     patient_preds_df = patient_preds_df.sort_values(by='loss')
-    output_path.mkdir(parents=True, exist_ok=True)
-    patient_preds_df.to_csv(preds_csv, index=False)
-
-
-if __name__ == '__main__':
-    from fire import Fire
-    Fire({
-        'train': train_categorical_model_cli,
-        'deploy': deploy_categorical_model_cli,
-    })
+    return patient_preds_df
