1cb4bf110a2117928933182b72a95cb2a0b1c537
Marko van Treeck <mvantreeck@ukaachen.de>
2022-04-26 15:16:25 +0200

    Add ROC visualization

---
+++ marugoto/visualizations/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Marko van Treeck'
+__copyright__ = 'Copyright 2022, Kather Lab'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__maintainer__ = 'Marko van Treeck'
+__email__ = 'mvantreeck@ukaachen.de'
+
+
+__changelog__ = {
+}
