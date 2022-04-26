c1c5ef74e27c0e114edc78dc5a52772604154f2f
Marko van Treeck <mvantreeck@ukaachen.de>
2022-04-26 16:33:26 +0200

    Add PRC, visualization titles

--- marugoto/__init__.py
+++ marugoto/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Marko van Treeck'
 __copyright__ = 'Copyright 2022, Kather Lab'
 __license__ = 'MIT'
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __maintainer__ = 'Marko van Treeck'
 __email__ = 'mvantreeck@ukaachen.de'
 
@@ -13,4 +13,5 @@ __changelog__ = {
     '0.5.0': 'Update marugoto.mil from 0.1.0 to 0.2.0',
     '0.5.0': 'Update marugoto.mil from 0.2.0 to 0.3.0',
     '0.6.0': 'Add marugoto.visualizations.roc',
+    '0.7.0': 'Add marugoto.visualizations.prc',
 }
--- marugoto/visualizations/prc.py
+++ marugoto/visualizations/prc.py
@@ -5,20 +5,20 @@ from typing import Iterable, Sequence, Optional, Tuple
 import numpy as np
 import scipy.stats as st
 from matplotlib import pyplot as plt
-from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import precision_recall_curve, average_precision_score
 
 
-all = ['plot_roc_curve', 'plot_roc_curves', 'plot_roc_curves_']
+all = ['plot_precision_recall_curve', 'plot_precision_recall_curves', 'plot_precision_recall_curves_']
 
 
-def plot_roc_curve(
+def plot_precision_recall_curve(
     ax: plt.Axes,
     y_true: Sequence[int],
     y_pred: Sequence[float],
     *,
     title: Optional[str] = None,
 ) -> int:
-    """Plots a single ROC curve.
+    """Plots a single precision-recall curve.
 
     Args:
         ax:  Axis to plot to.
@@ -29,12 +29,12 @@ def plot_roc_curve(
     Returns:
         The area under the curve.
     """
-    fpr, tpr, _ = roc_curve(y_true, y_pred)
+    fpr, tpr, _ = precision_recall_curve(y_true, y_pred)
     ax.plot(fpr, tpr)
 
-    style_auc(ax)
+    style_auc(ax, baseline=y_true.sum()/len(y_true))
 
-    auc = roc_auc_score(y_true, y_pred)
+    auc = average_precision_score(y_true, y_pred)
     if title:
         ax.set_title(f'{title}\n(AUC = {auc:0.2f})')
     else:
@@ -43,24 +43,25 @@ def plot_roc_curve(
     return auc
 
 
-def style_auc(ax):
-    ax.plot([0, 1], [0, 1], 'r--')
+def style_auc(ax, baseline: float):
+    ax.plot([0, 1], [0, 1], 'r--', alpha=0)
     ax.set_aspect('equal')
-    ax.set_xlabel('False Positive Rate')
-    ax.set_ylabel('True Positive Rate')
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.plot([0, 1], [baseline, baseline], 'r--')
 
 
 TPA = namedtuple('TPA', ['true', 'pred', 'auc'])
 
 
-def plot_roc_curves(
+def plot_precision_recall_curves(
     ax: plt.Axes,
     y_trues: Sequence[Sequence[int]],
     y_preds: Sequence[Sequence[float]],
     *,
     title: Optional[str] = None
 ) -> Tuple[float, float]:
-    """Plots a family of ROC curves.
+    """Plots a family of precision-recall curves.
 
     Args:
         ax:  Axis to plot to.
@@ -72,17 +73,19 @@ def plot_roc_curves(
         The 95% confidence interval of the area under the curve.
     """
     # sort trues, preds, AUCs by AUC
-    tpas = [TPA(t, p, roc_auc_score(t, p))
+    tpas = [TPA(t, p, average_precision_score(t, p))
             for t, p in zip(y_trues, y_preds)]  # , strict=True)]
     tpas = sorted(tpas, key=lambda x: x.auc, reverse=True)
 
-    # plot rocs
+    # plot precision_recalls
     for t, p, auc in tpas:
-        fpr, tpr, _ = roc_curve(t, p)
-        ax.plot(fpr, tpr, label=f'AUC = {auc:0.2f}')
+        precision, recall, _ = precision_recall_curve(t, p)
+        ax.plot(recall, precision, label=f'AUC = {auc:0.2f}')
 
     # style plot
-    style_auc(ax)
+    all_samples = np.concatenate(y_trues)
+    baseline = all_samples.sum()/len(all_samples)
+    style_auc(ax, baseline=baseline)
     ax.legend()
 
     # calculate confidence intervals and print title
@@ -100,15 +103,15 @@ def plot_roc_curves(
     return l, h
 
 
-def plot_roc_curves_(
+def plot_precision_recall_curves_(
         *pred_csvs: Iterable[str], target_label: str, true_label: str, outpath
 ) -> None:
-    """Creates ROC curves.
+    """Creates precision-recall curves.
 
     Args:
         pred_csvs:  A list of prediction CSV files.
-        target_label:  The target label to calculate the ROC for.
-        true_label:  The positive class for the ROC.
+        target_label:  The target label to calculate the precision-recall for.
+        true_label:  The positive class for the precision-recall.
         outpath:  Path to save the `.svg` to.
     """
     import pandas as pd
@@ -118,15 +121,15 @@ def plot_roc_curves_(
     y_trues = [df[target_label] == true_label for df in pred_dfs]
     y_preds = [df[f'{target_label}_{true_label}'] for df in pred_dfs]
     title = f'{target_label} = {true_label}'
-
     fig, ax = plt.subplots()
     if len(pred_dfs) == 1:
-        plot_roc_curve(ax, y_trues[0], y_preds[0], title=title)
+        plot_precision_recall_curve(ax, y_trues[0], y_preds[0], title=title)
     else:
-        plot_roc_curves(ax, y_trues, y_preds, title=title)
-    fig.savefig(Path(outpath)/f'roc-{target_label}={true_label}.svg')
+        plot_precision_recall_curves(ax, y_trues, y_preds, title=title)
+
+    fig.savefig(Path(outpath)/f'prc-{target_label}={true_label}.svg')
 
 
 if __name__ == '__main__':
     from fire import Fire
-    Fire(plot_roc_curves_)
+    Fire(plot_precision_recall_curves_)
