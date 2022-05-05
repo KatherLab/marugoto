64de94ba2faf539db09a28283ab2ae501c015dbd
Marko van Treeck <mvantreeck@ukaachen.de>
2022-05-05 16:33:53 +0200

    Bump version to 0.8.0

--- marugoto/__init__.py
+++ marugoto/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Marko van Treeck'
 __copyright__ = 'Copyright 2022, Kather Lab'
 __license__ = 'MIT'
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 __maintainer__ = 'Marko van Treeck'
 __email__ = 'mvantreeck@ukaachen.de'
 
@@ -14,4 +14,5 @@ __changelog__ = {
     '0.5.0': 'Update marugoto.mil from 0.2.0 to 0.3.0',
     '0.6.0': 'Add marugoto.visualizations.roc',
     '0.7.0': 'Add marugoto.visualizations.prc',
+    '0.8.0': '.mil 0.3.0->0.4.0, .data 0.1.0->0.2.0, .visualizations 0.2.0->0.2.1',
 }
