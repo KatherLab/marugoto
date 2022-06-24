a92b94932a45b68675a87c9aaff9389826141dc9
Marko van Treeck <markovantreeck@gmail.com>
2022-06-24 15:31:25 +0200

    Add extractor information to tiles.

--- marugoto/extract.py
+++ marugoto/extract.py
@@ -1,9 +1,10 @@
 # %%
 import os
 import re
+import json
 from typing import Optional
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, ConcatDataset
 from pathlib import Path
 import PIL
 from torchvision import models, transforms
@@ -12,13 +13,23 @@ import numpy as np
 from tqdm import tqdm
 import h5py
 
+__author__ = 'Marko van Treeck'
+__copyright__ = 'Copyright 2022, Kather Lab'
+__license__ = 'MIT'
+__version__ = '0.2.0'
+__maintainer__ = 'Marko van Treeck'
+__email__ = 'mvantreeck@ukaachen.de'
+
+
+__changelog__ = {
+    '0.2.0': 'Add extractor information to tiles.',
+}
+
 __all__ = ['extract_features_']
 
 
-# %%
 class SlideTileDataset(Dataset):
     def __init__(self, slide_dir: Path, transform=None, *, repetitions: int = 1) -> None:
-        assert repetitions >= 1, 'at least one repetition of the dataset required'
         self.tiles = list(slide_dir.glob('*.jpg'))*repetitions
         assert self.tiles, f'no tiles found in {slide_dir}'
         self.transform = transform
@@ -45,10 +56,10 @@ def _get_coords(filename) -> Optional[np.ndarray]:
 
 def extract_features_(
     *slide_tile_paths: Path, outdir: Path, augmented_repetitions: int = 0,
-    model=None,
+    model=None, model_name=None,
 ) -> None:
     """Extracts features from slide tiles.
-    
+
     Args:
         slide_tile_paths:  A list of paths containing the slide tiles, one
             per slide.
@@ -61,7 +72,11 @@ def extract_features_(
     if not model:
         model = models.resnet18(pretrained=True)
         model.fc = nn.Identity()
-    model = model.half().eval().to(device)
+        model_name = 'resnet18-imagenet'
+    elif not model_name:
+        raise ValueError('`model_name` has to specified when supplying a custom model.')
+
+    model = model.eval().to(device)
     normal_transform = transforms.Compose([
         transforms.Resize(224),
         transforms.CenterCrop(224),
@@ -74,15 +89,20 @@ def extract_features_(
         transforms.RandomHorizontalFlip(p=.5),
         transforms.RandomVerticalFlip(p=.5),
         transforms.RandomApply([transforms.GaussianBlur(3)], p=.5),
-        transforms.RandomApply([transforms.ColorJitter(brightness=.1, contrast=.2, saturation=.25, hue=.125)], p=.5),
+        transforms.RandomApply([transforms.ColorJitter(
+            brightness=.1, contrast=.2, saturation=.25, hue=.125)], p=.5),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     outdir = Path(outdir)
     outdir.mkdir(exist_ok=True, parents=True)
+    extractor_string = f'marugoto-extract-v{__version__}_{model_name}'
+    with open(outdir/'info.json') as f:
+        json.dump(f, {'extractor': extractor_string})
 
     for slide_tile_path in tqdm(slide_tile_paths):
         slide_tile_path = Path(slide_tile_path)
+        # check if h5 for slide already exists / slide_tile_path path contains tiles
         if (h5outpath := outdir/f'{slide_tile_path.name}.h5').exists():
             print(f'{h5outpath} already exists.  Skipping...')
             continue
@@ -90,26 +110,28 @@ def extract_features_(
             print(f'No tiles in {slide_tile_path}.  Skipping...')
             continue
 
-        ds = SlideTileDataset(slide_tile_path, normal_transform)
-        _extract(ds, model, device, h5outpath)
+        ds = ConcatDataset([
+            SlideTileDataset(slide_tile_path, normal_transform),
+            SlideTileDataset(slide_tile_path, augmenting_transform,
+                                repetitions=augmented_repetitions),
+        ])
+        dl = torch.utils.data.DataLoader(
+            ds, batch_size=64, shuffle=False, num_workers=os.cpu_count(), drop_last=False)
 
-        if augmented_repetitions:
-            ds = SlideTileDataset(
-                slide_tile_path, augmenting_transform, repetitions=augmented_repetitions-1)
-            _extract(ds, model, device, h5outpath)
+        feats = []
+        for batch in tqdm(dl, leave=False):
+            feats.append(model(batch.half().to(device)).half().cpu().detach())
 
-
-def _extract(ds, model, device, h5outpath):
-    dl = torch.utils.data.DataLoader(ds, batch_size=64, shuffle=False, num_workers=os.cpu_count(), drop_last=False)
-    feats = []
-    for batch in tqdm(dl, leave=False):
-        feats.append(model(batch.half().to(device)).detach())
-
-    with h5py.File(h5outpath, 'w') as f:
-        f['coords'] = [_get_coords(fn) for fn in ds.tiles]
-        f['feats'] = torch.concat(feats).cpu().numpy()
+        with h5py.File(h5outpath, 'w') as f:
+            f['coords'] = [_get_coords(fn) for fn in ds.tiles]
+            f['feats'] = torch.concat(feats).cpu().numpy()
+            f['augmented'] = np.repeat([False, True], [len(ds.datasets[0]), len(ds.datasets[1])])
+            assert len(f['feats']) == len(f['augmented'])
+            f.attrs['extractor'] = extractor_string
 
 
 if __name__ == '__main__':
     import fire
     fire.Fire(extract_features_)
+
+# %%
