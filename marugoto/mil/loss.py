7982579e2357eafbc5266c0f464e856e36a40328
Avic3nna <omarelnahhas1337@gmail.com>
2022-11-11 11:26:57 +0100

    added batch monte carlo mse loss

--- marugoto/mil/loss.py
+++ marugoto/mil/loss.py
@@ -3,6 +3,37 @@ import torch.nn.functional as F
 import torch.nn as nn
 from torch.autograd import Variable
 
+# https://github.com/jiawei-ren/BalancedMSE
+class BMCLoss(nn.Module):
+    def __init__(self, init_noise_sigma):
+        super(BMCLoss, self).__init__()
+        self.noise_sigma = torch.nn.Parameter(torch.tensor(init_noise_sigma))
+
+    def forward(self, pred, target):
+        noise_var = self.noise_sigma ** 2
+        return bmc_loss(pred, target, noise_var)
+
+def bmc_loss(pred, target, noise_var):
+    """Compute the Balanced MSE Loss (BMC) between `pred` and the ground truth `targets`.
+    Args:
+      pred: A float tensor of size [batch, 1].
+      target: A float tensor of size [batch, 1].
+      noise_var: A float number or tensor.
+    Returns:
+      loss: A float tensor. Balanced MSE Loss.
+    """
+    target = target[0]
+    #print("Calculating BMC loss...")
+    #breakpoint()
+    logits = - (pred - target.T).pow(2) / (2 * noise_var)   # logit size: [batch, batch]
+    loss = F.cross_entropy(logits, torch.arange(pred.shape[0]))     # contrastive-like loss
+    loss = loss * (2 * noise_var) #.detach()  # optional: restore the loss scale, 'detach' when noise is learnable 
+
+    return loss
+
+
+
+
 class WeightedMSELoss(nn.Module):
     def __init__(self, **kwargs): #weights=None,
         super().__init__()
